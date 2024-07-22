f85c97c41e79affc2df55a90214855fd378f571e
Rikitav Tim4ik <gutii.ip.pl@gmail.com>
2024-07-22 22:09:23 +0400

    added builder batch

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set projName=rEFInd-Automenu.ConsoleApplication
+set profilesDir=%projName%\Properties\PublishProfiles\
+
+echo Building ARM64_Standalone...
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_ARM64_Standalone.pubxml > nul
+echo Building x64_Standalone...
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x64_Standalone.pubxml > nul
+echo Building x86_Standalone...
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x86_Standalone.pubxml > nul
+
+echo Archiving...
+7z a -tzip -mx3 %cd%\bin\console\win-arm64-standalone.zip %cd%\bin\console\win-arm64-s\refind.exe > nul
+7z a -tzip -mx3 %cd%\bin\console\win-x64-standalone.zip %cd%\bin\console\win-x64-s\refind.exe > nul
+7z a -tzip -mx3 %cd%\bin\console\win-x86-standalone.zip %cd%\bin\console\win-x86-s\refind.exe > nul
