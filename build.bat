d6a6f2ff4ea89b1fd2210cabd4a3ed139aa98dbb
Tim4ik <gutii.ip.pl@gmail.com>
2024-08-10 21:27:48 +0400

    update 63.1
    minor build script changes

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
 
 set projName=rEFInd-Automenu.ConsoleApplication
-set profilesDir=%projName%\Properties\PublishProfiles\
+set profilesDir=%projName%\Properties\PublishProfiles
+set buildLogsDir=%cd%\bin\console\BuildLogs
 
 echo Building x64_Standalone...
-dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x64_Standalone.pubxml > nul
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x64_Standalone.pubxml > buildLog_x64.log
 echo Building x86_Standalone...
-dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x86_Standalone.pubxml > nul
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_x86_Standalone.pubxml > buildLog_x86.log
 echo Building ARM64_Standalone...
-dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_ARM64_Standalone.pubxml > nul
+dotnet publish %projName%\%projName%.csproj /p:PublishProfile=%profilesDir%\FolderProfile_ARM64_Standalone.pubxml > buildLog_aa64.log
 
 echo Archiving...
 7z a -tzip -mx3 %cd%\bin\console\win-arm64-standalone.zip %cd%\bin\console\win-arm64-s\refind.exe > nul
